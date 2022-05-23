373f29bb68b203964d888d6e85581cba7b801a99
Lucas Viana <lucasvfv@gmail.com>
2022-05-23 13:55:55 -0300

    segundo commit

--- index.js
+++ index.js
@@ -5,20 +5,17 @@ import multer from "multer";
 
 const app = express();
 app.use(cors())
-// app.use(express.json())
+app.use(express.json())
 
 const upload = multer({ dest: 'temp/' });
 
 app.post('/upload', upload.single('img'), (req, res) => {
     const file = req.file;
     const body = req.body;
+
+
     console.log(body, file)
-    res.status(200);
-})
-
-
-app.get('/upload', (req, res) => {
-    res.sendStatus(200)
+    res.status(200).send(file);
 })
 
 app.listen("5000", () => console.log("Running..."))
